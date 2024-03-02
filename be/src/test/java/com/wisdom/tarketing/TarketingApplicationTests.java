113627a06e39e0b51d45c1506a9d4529b7db00dc
JIHYE <jdh324@naver.com>
2024-03-02 18:11:48 +0900

    chore: 프로젝트 초기 설정 #1

---
+++ be/src/test/java/com/wisdom/tarketing/TarketingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wisdom.tarketing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TarketingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
