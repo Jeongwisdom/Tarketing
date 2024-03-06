f54bf8dce79899d0969e1e5cf75e5a983ca7db48
JIHYE <jdh324@naver.com>
2024-03-06 17:01:39 +0900

    feat: databaseCleaner 구현 #4

---
+++ be/src/test/java/com/wisdom/tarketing/test_support/DatabaseCleanup.java
@@ -0,0 +1,42 @@
+package com.wisdom.tarketing.test_support;
+
+import jakarta.persistence.EntityManager;
+import java.sql.DatabaseMetaData;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+import javax.sql.DataSource;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Component
+public class DatabaseCleanup implements InitializingBean {
+
+    private final List<String> tableNames = new ArrayList<>();
+    @Autowired
+    private DataSource dataSource;
+    @Autowired
+    private EntityManager entityManager;
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        DatabaseMetaData metaData = dataSource.getConnection().getMetaData();
+        ResultSet tables = metaData.getTables("tarketing", null, null, new String[]{"TABLE"});
+        while (tables.next()) {
+            String tableName = tables.getString("TABLE_NAME");
+            tableNames.add(tableName);
+        }
+    }
+
+    @Transactional
+    public void execute() {
+        entityManager.flush();
+        for (String tableName : tableNames) {
+            entityManager.createNativeQuery(String.format("TRUNCATE TABLE `%s`", tableName)).executeUpdate();
+            entityManager.createNativeQuery(String.format("ALTER TABLE %s ALTER COLUMN id RESTART WITH 1", tableName))
+                    .executeUpdate();
+        }
+    }
+}
